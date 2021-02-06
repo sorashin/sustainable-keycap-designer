dba3b320b1a8e632c2784002f89e8e488f2ac1d1
sorashin <naka52shin@gmail.com>
2021-02-07 00:52:09 +0900

    initial three.js setup

---
+++ src/scenes/BasicScene.js
@@ -0,0 +1,65 @@
+import React, { useEffect, createRef } from 'react'
+import * as THREE from 'three'
+
+
+const newScene = () => {
+  const scene = new THREE.Scene()
+  return scene
+}
+
+const newCamera = () => {
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.x = 100
+  camera.position.y = 100
+  camera.position.z = 400
+  return camera
+}
+
+const newRenderer = (mount) => {
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.autoClear = true
+  if (mount.current) {
+    mount.current.appendChild(renderer.domElement)
+  }
+  return renderer
+}
+
+const BaseScene = () => {
+  const mount = createRef()
+  useEffect(() => {
+    // scene
+    const scene = newScene()
+
+    // camera
+    const camera = newCamera()
+
+    // renderer
+    const renderer = newRenderer(mount)
+
+    // mesh
+    const geometry = new THREE.BoxGeometry(50, 50, 50)
+    const material = new THREE.MeshNormalMaterial()
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+    // render
+    const render = () => {
+      renderer.render(scene, camera)
+    }
+
+    // animation
+    const animate = () => {
+      requestAnimationFrame(animate)
+      render()
+    }
+    animate()
+  }, [mount])
+  return (
+    <>
+      <div ref={mount} />
+    </>
+  )
+}
+export default BaseScene
