47605340d24eebda6ce81597aaa75ff7873c49ce
sorashin <naka52shin@gmail.com>
2021-02-07 00:02:27 +0900

    WIP adding script

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React, {useState, useEffect} from "react"
+import threeJs from '../components/three.js'
 
 // styles
 const pageStyles = {
@@ -55,6 +56,12 @@ const docLink = {
 
 // markup
 const IndexPage = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    threeJs(() => {
+      setLoaded(true);
+    });
+  });
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
@@ -88,6 +95,15 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+      <canvas className="webgl"></canvas>
+      <div className="loading-bar"></div>
+      <div className="info">
+          <div className="thumbnail"></div>
+          <div className="text">
+              <div className="title">タイトル</div>
+              <div className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt molestias assumenda officiis reprehenderit quasi fuga rem fugit sed ullam illum dicta quae, atque impedit fugiat, quo saepe quas! Perferendis, architecto.</div>
+          </div>
+      </div>
     </main>
   )
 }
